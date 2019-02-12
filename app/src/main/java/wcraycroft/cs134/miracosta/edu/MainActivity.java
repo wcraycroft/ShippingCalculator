072addd6c1ebd61c8a89731c1dfd787db0238516
wcraycroft <wrcraycroft@gmail.com>
2019-02-12 01:00:17 -0800

    Added javadoc. Added background and icon images.

--- app/src/main/java/wcraycroft/cs134/miracosta/edu/MainActivity.java
+++ app/src/main/java/wcraycroft/cs134/miracosta/edu/MainActivity.java
@@ -1,3 +1,10 @@
+/**
+ * Controller class for the Shipping Calculator's main activity.
+ * This activity allows the user to enter an item weight in ounces via EditText.
+ * When the EditText is changed, the controller will send new weight to the model,
+ * and will update cost information.
+ */
+
 package wcraycroft.cs134.miracosta.edu;
 
 import android.support.v7.app.AppCompatActivity;
@@ -15,18 +22,29 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Declarations / Member variables
-    // Text format
+    /** Formats all cost output to local currency (values are not converted) */
     NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.getDefault());
-    // Views
+    /** EditText view used for user input*/
     private EditText weightEditText;
+    /** TextView used to output base cost*/
     private TextView baseCostTextView;
+    /** TextView used to output added cost*/
     private TextView addedCostTextView;
+    /** TextView used to output total cost*/
     private TextView totalCostTextView;
-    // Models
+    /** Model class used to calculate shipping costs. */
     private ShipItem currentItem;
 
     @Override
+    /**
+     *  Called at beginning of activity lifecycle.
+     *  Initializes main layout.
+     *  Instantiates ShipItem model with default values.
+     *  Instantiates and links Views.
+     *  Adds TextChangedListener to EditText view.
+     *
+     * @param savedInstanceState Saved data from previous state. Currently, no data is saved.
+     *  */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -50,16 +68,23 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
+            /**
+             * Called when user makes a change to weightEditText.
+             * Tries to get integer input from EditText, catches NumberFormatExceptions.
+             * Sends value to model, get calculated data and outputs it to view.
+             *
+             * @param s Current CharSequence in EditText
+             */
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 // Get new weight from view, send to model
                 try {
                     currentItem.setWeight(Integer.parseInt(s.toString()));
                 } catch (NumberFormatException e) {
                     Log.w("ShippingCalculator", "NumberFormatException caught in MainActivity.java @ line 54 (parsing empty string?)");
-                    // If string is empty, set weight to 0 in model
+                    // If string is empty, send weight of 0 to model.
                     currentItem.setWeight(0);
                 }
-                // Get calculated info from model, send to view
+                // Get calculated info from model, send to view.
                 baseCostTextView.setText(currency.format(currentItem.getBaseCost()));
                 addedCostTextView.setText(currency.format(currentItem.getAddedCost()));
                 totalCostTextView.setText(currency.format(currentItem.getTotalCost()));
--- app/src/main/java/wcraycroft/cs134/miracosta/edu/ShipItem.java
+++ app/src/main/java/wcraycroft/cs134/miracosta/edu/ShipItem.java
@@ -1,51 +1,93 @@
 package wcraycroft.cs134.miracosta.edu;
 
+/**
+ * Represents an item to be shipped.
+ * Takes in a weight in ounces and uses a static formula to calculate shipping cost.
+ */
 public class ShipItem {
+
     // Constants
+    /** The base cost to ship an item under minimum weight */
     private static final double BASE_COST = 3.0;
-    private static final double ADDED_COST_PER_WEIGHT = 0.5;
+    /** The added cost to be multiplied for each weight increment above minimum weight*/
+    private static final double ADDED_COST_PER_INCREMENT = 0.5;
+    /** The minimum weight the item can be before added costs are applied*/
     private static final int MINIMUM_WEIGHT = 16;
-    private static final int ADDED_COST_WEIGHT = 4;
+    /** The weight increment at which additional added costs are applied.
+     *  Note that weight thresholds are exclusive.*/
+    private static final int ADDED_COST_INCREMENT = 4;
     // Member variables
+    /** The item's weight in ounces.*/
     private int mWeight;
+    /** The item's added costs above base cost.*/
     private double mAddedCost;
+    /** The item's total cost (base cost + added cost)*/
     private double mTotalCost;
 
-    // Constructor
+    /**
+     * Creates a new shipping item with a default weight of 0.
+     * Setter will instantiate other member variables to appropriate values.
+     */
     public ShipItem() {
-        mWeight = 0;
-        calculateCosts();
+        setWeight(0);
     }
 
-    // Getters
+    /**
+     * Gets the item's weight in onces.
+     *
+     * @return The item's weight.
+     */
     public int getWeight() {
         return mWeight;
     }
 
+    /**
+     * Gets the item's added costs when it exceeds minimum weight.
+     *
+     * @return The added cost.
+     */
     public double getAddedCost() {
         return mAddedCost;
     }
 
+    /**
+     * Gets the item's base cost, a predetermined constant.
+     *
+     * @return The base cost.
+     */
     public double getBaseCost() {
         return BASE_COST;
     }
 
+    /**
+     * Gets the item's total cost (base cost + added costs)
+     *
+     * @return The total cost.
+     */
     public double getTotalCost() {
         return mTotalCost;
     }
 
-    // Setters
+    /**
+     * Changes the item's weight and recalculates it's added and total costs.
+     *
+     * @param weight The item's new weight.
+     */
     public void setWeight(int weight) {
         mWeight = weight;
         calculateCosts();
     }
 
-    // Calculate added cost and total cost
+    /**
+     * Calculates the added and total costs based on the current weight.
+     * This method is called when ShipItem is instantiated or when setWeight() is called.
+     * If weight is below minimum weight, sets total cost to base cost.
+     * Because weight thresholds are exclusive, minimum weight is reduced by 1 in the added
+     * cost equation prior to integer division.
+     */
     private void calculateCosts() {
-        // If over min weight for added costs.
-        // Because weight thresholds are exclusive, subtract 1 from weight before int division.
-        mAddedCost = (mWeight > MINIMUM_WEIGHT) ? (((mWeight - MINIMUM_WEIGHT - 1) / ADDED_COST_WEIGHT)
-                * ADDED_COST_PER_WEIGHT) + ADDED_COST_PER_WEIGHT : 0.0;
+        mAddedCost = (mWeight > MINIMUM_WEIGHT) ? (((mWeight - MINIMUM_WEIGHT - 1) / ADDED_COST_INCREMENT)
+                * ADDED_COST_PER_INCREMENT) + ADDED_COST_PER_INCREMENT : 0.0;
         mTotalCost = BASE_COST + mAddedCost;
     }
 }
